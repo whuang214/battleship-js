e1e25a601ff560dc89fe050be7c22da07ba5ca39
whuang214 <whuang2146@gmail.com>
2023-06-16 10:43:09 -0400

    added user stories, wireframe, some base html/css code

---
+++ js/app.js
@@ -0,0 +1,8 @@
+console.log("app.js loaded");
+
+document.getElementById("playButton").addEventListener("click", function () {
+  document.querySelector(".overlay").style.display = "none";
+});
+
+
+// 
